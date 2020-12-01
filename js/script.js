a11197cc666b0c91bdd17f2743690545cf879e9b
tomas-ondrejka <tomas.ondrejka2000@gmail.com>
2020-12-01 12:55:25 +0100

    we

--- js/script.js
+++ js/script.js
@@ -1,10 +1,26 @@
 $("body").mousemove(function (e) {
     $('#net').css({'top': e.clientY + 4, 'left': e.clientX + 4});
 });
-$('.apple').click(function () {
-    $(this).animate({top: "85%", left: "27%"}, 900)
+var off = 0;
+$('.apple').on("click", function () {
+    $(this).animate({top: '85%', left: 23 + off+ '%'}   , 700);
+    off+=2;
 });
-$("#wateringcan").click(function () {
-    $(this).rotate(40);
-    $(this).animate({top: "85%", left: "27%"}, 900)
+
+function randomNumber(min, max) {
+    var temp = Math.round(Math.random() * max) + min + '%';
+    return temp;
+}
+
+
+var i = true;
+$("#wateringcan").on("click", function () {
+    if (i) {
+        $(this).css({'transform': 'rotate(-20deg)'})
+        i = false;
+    } else {
+        $(this).css({'transform': 'rotate(0deg)'})
+        i = true;
+    }
+
 });
